1c2a08f9e8abda70135bbdb21a868f754f1529b9
prdex <bambhania.pratik9090@gmail.com>
2020-10-15 12:38:01 +0530

    New files

--- Std10/insetmaterial_Std10.py
+++ Std10/insetmaterial_Std10.py
@@ -7,10 +7,12 @@ gujarati = "%E0%AA%97%E0%AB%81%E0%AA%9C%E0%AA%B0%E0%AA%BE%E0%AA%A4%E0%AB%80/"
 science = "%E0%AA%B5%E0%AA%BF%E0%AA%9C%E0%AB%8D%E0%AA%9E%E0%AA%BE%E0%AA%A8/"
 scoial = "%E0%AA%B8%E0%AA%BE%E0%AA%AE%E0%AA%BE%E0%AA%9C%E0%AA%BF%E0%AA%95%20%E0%AA%B5%E0%AA%BF%E0%AA%9C%E0%AB%8D%E0%AA%9E%E0%AA%BE%E0%AA%A8/"
 
+linkvar  = "https://drive.google.com/uc?id=1ZUH6oXz5T_qRb5UCDIYCV6p6g1TKsP9R&export=download"
+namevar = "ગુજરાતી પ્રકરણ ૧૧"
 
 data = {
-    'downloadlink':'link',
-    'name':"name"
+    'downloadlink':linkvar,
+    'name':namevar
 }
 standard1 = "/std10A/"
 print("Enter subject code")
@@ -29,7 +31,7 @@ for x in range(2):
         firebaseref.delete(standard1+'materiallink/'+english+firstelementindex, stringdelete)  
         standard1 = "/std10B/"
         
-    elif subjectindex == "2"
+    elif subjectindex == "2":
         result = firebaseref.get(standard1+"materiallink/"+maths,'')
         dict_items = result.items()
         firstelement = list(dict_items)[:1]
@@ -42,20 +44,20 @@ for x in range(2):
         firebaseref.delete(standard1+'materiallink/'+maths+firstelementindex, stringdelete)  
         standard1 = "/std10B/"
     
-     elif subjectindex == "3"
+    elif subjectindex == "3":
         result = firebaseref.get(standard1+"materiallink/"+gujarati,'')
         dict_items = result.items()
         firstelement = list(dict_items)[:1]
         firstelementindex = int(firstelement[0][0])-1
         firstelementindex = str(firstelementindex)
         result = firebaseref.post(standard1+'materiallink/'+gujarati+firstelementindex, data)
-        firebaseref.put(standard1+'materiallink/'+gujarati+firstelementindex,'downloadlink','testabc')
-        firebaseref.put(standard1+'materiallink/'+gujarati+firstelementindex,'name','abcname')
+        firebaseref.put(standard1+'materiallink/'+gujarati+firstelementindex,'downloadlink', linkvar)
+        firebaseref.put(standard1+'materiallink/'+gujarati+firstelementindex,'name',namevar)
         stringdelete = result["name"]
         firebaseref.delete(standard1+'materiallink/'+gujarati+firstelementindex, stringdelete)  
         standard1 = "/std10B/"
     
-     elif subjectindex == "4"
+    elif subjectindex == "4":
         result = firebaseref.get(standard1+"materiallink/"+science,'')
         dict_items = result.items()
         firstelement = list(dict_items)[:1]
@@ -68,7 +70,7 @@ for x in range(2):
         firebaseref.delete(standard1+'materiallink/'+science+firstelementindex, stringdelete)  
         standard1 = "/std10B/"
     
-    elif subjectindex == "5"
+    elif subjectindex == "5":
         result = firebaseref.get(standard1+"materiallink/"+scoial,'')
         dict_items = result.items()
         firstelement = list(dict_items)[:1]
---
+++ Std12A/insertmaterial_Std12A.py
@@ -0,0 +1,65 @@
+from firebase import firebase
+from firebase import firebase
+from datetime import date
+import sys
+
+firebaseref = firebase.FirebaseApplication("https://njvidhyalay-4f0ea.firebaseio.com")
+
+english = "%E0%AA%85%E0%AA%82%E0%AA%97%E0%AB%8D%E0%AA%B0%E0%AB%87%E0%AA%9C%E0%AB%80/"
+gujarati = "%E0%AA%97%E0%AB%81%E0%AA%9C%E0%AA%B0%E0%AA%BE%E0%AA%A4%E0%AB%80/"
+eco     = "%E0%AA%85%E0%AA%B0%E0%AB%8D%E0%AA%A5%E0%AA%B6%E0%AA%BE%E0%AA%B8%E0%AB%8D%E0%AA%A4%E0%AB%8D%E0%AA%B0/"
+
+today = date.today()
+d1 = today.strftime("%d/%m/%Y")
+
+linkvar = "linktest"
+namevar = "namevar"
+
+data = {
+    'downloadlink':linkvar,
+    'name':namevar
+}
+standard1 = "/std11A/"
+
+print("Enter subject code")
+subjectindex = input().strip()
+
+for x in range(2):
+    if subjectindex == "1":
+        result = firebaseref.get(standard1+"materiallink/"+english,'')
+        dict_items = result.items()
+        firstelement = list(dict_items)[:1]
+        firstelementindex = int(firstelement[0][0])-1
+        firstelementindex = str(firstelementindex)
+        result = firebaseref.post(standard1+'materiallink/'+english+firstelementindex, data)
+        firebaseref.put(standard1+'materiallink/'+english+firstelementindex,'downloadlink',linkvar)
+        firebaseref.put(standard1+'materiallink/'+english+firstelementindex,'name',namevar)
+        stringdelete = result["name"]
+        firebaseref.delete(standard1+'materiallink/'+english+firstelementindex, stringdelete)  
+        standard1 = "/std11C/"
+    
+    elif subjectindex == "2":
+        result = firebaseref.get(standard1+"materiallink/"+gujarati,'')
+        dict_items = result.items()
+        firstelement = list(dict_items)[:1]
+        firstelementindex = int(firstelement[0][0])-1
+        firstelementindex = str(firstelementindex)
+        result = firebaseref.post(standard1+'materiallink/'+gujarati+firstelementindex, data)
+        firebaseref.put(standard1+'materiallink/'+english+firstelementindex,'downloadlink',linkvar)
+        firebaseref.put(standard1+'materiallink/'+english+firstelementindex,'name',namevar)
+        stringdelete = result["name"]
+        firebaseref.delete(standard1+'materiallink/'+gujarati+firstelementindex, stringdelete)  
+        standard1 = "/std11C/"
+    
+    elif subjectindex == "3":
+        result = firebaseref.get(standard1+"materiallink/"+eco,'')
+        dict_items = result.items()
+        firstelement = list(dict_items)[:1]
+        firstelementindex = int(firstelement[0][0])-1
+        firstelementindex = str(firstelementindex)
+        result = firebaseref.post(standard1+'materiallink/'+eco+firstelementindex, data)
+        firebaseref.put(standard1+'materiallink/'+english+firstelementindex,'downloadlink',linkvar)
+        firebaseref.put(standard1+'materiallink/'+english+firstelementindex,'name',namevar)
+        stringdelete = result["name"]
+        firebaseref.delete(standard1+'materiallink/'+eco+firstelementindex, stringdelete)  
+        standard1 = "/std11C/"
